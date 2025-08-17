7d2fb0ade5c86c7aba1e7aa5e6bf5b3d8a757b3b
Wilfredo Villegas <wvillegasm@gmail.com>
2025-08-17 16:16:33 -0400

    Fix configurations - Comment from PR

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,16 @@
 import react from '@vitejs/plugin-react-swc';
+import * as fs from 'fs';
+import * as path from 'path';
 import { defineConfig } from 'vitest/config';
+const tsconfigPath = path.resolve(__dirname, './tsconfig.test.json');
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    typecheck: {
-      tsconfig: './tsconfig.test.json'
-    }
+    typecheck: fs.existsSync(tsconfigPath)
+      ? { tsconfig: './tsconfig.test.json' }
+      : {},
   }
 })
